18efb73fd7ddba39f4712a4870c568eae9e7ad02
Ammar Ratnani <ammrat13@gmail.com>
2022-12-04 23:24:40 -0500

    Add docs for spin

--- src/flush_reload_test_pkg/primitives.nim
+++ src/flush_reload_test_pkg/primitives.nim
@@ -9,8 +9,9 @@
 ## cycles. That's not implemented in assembly since testing shows that it
 ## doesn't make much of a difference to performance.
 
-
 {.compile: "./primitives.s".}
+
+
 proc rdtsc*(): uint32 {.importc.}
   ## Read the timestamp counter
   ##
@@ -29,17 +30,31 @@ proc rdtsc*(): uint32 {.importc.}
 proc probe*(a: ref or ptr): uint32 {.importc.}
   ## Probe the time it takes to access an address
   ##
-  ## This code access the address passed to it as the first parameter. It times
-  ## how long it takes the processor to access it, then flushes it from the
-  ## cache. Only the least significant 32 bits are used in timing. The timer
+  ## This code accesses the address passed to it as the first parameter. It
+  ## times how long it takes the processor to access it, then flushes it from
+  ## the cache. Only the least significant 32 bits are used in timing. The timer
   ## will overflow after 1.43s at 3.0GHz. However, memory accesses should be
   ## much faster than that.
+  ##
+  ## Note: The code assumes that `ref`s are treated the same way `ptr`s are in
+  ## function calls. Specifically, it expects a raw pointer to the object to be
+  ## passed in register `%rax`. If Nim's calling convention changes, this
+  ## function will likely have to be fixed.
   ##
   ## See:
   ##  * FLUSH+RELOAD Paper
   ##  * `rdtsc <#rdtsc>`_
 
 proc spin*(n: uint32) =
+  ## Spinlock until a certain number of cycles have passed
+  ##
+  ## This code continuously reads the timestamp counter until it reads at least
+  ## `n` more than it did when the subroutine first called `rdtsc()`. In other
+  ## words, it spins for at least `n` cycles.
+  ##
+  ## Note: The value for `n` should not be too close to `high(uint32)`.
+  ## Otherwise, the timer could overflow, causing the code to miss the fact that
+  ## `n` cycles have elapsed.
   let start_time = rdtsc()
   while rdtsc() - start_time < n:
     continue
