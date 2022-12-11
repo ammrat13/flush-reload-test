c5fe48bb1083f6b2891525e32cfabffbb33d2705
Ammar Ratnani <ammrat13@gmail.com>
2022-12-10 23:04:58 -0500

    Change addresses in run.sh

--- run.sh
+++ run.sh
@@ -5,17 +5,16 @@ export GNUPGHOME="${PWD}/gnupg-1.4.13/home/"
 
 GNUPG="./gnupg-1.4.13/install/bin/gpg"
 SFILE="README.md"
-OFILE="run.sh.csv"
 
 rm -f "${SFILE}.sig"
 
-nimble build -d:release
+nimble build -d:release >&2
 
 taskset 0x1 ./flush_reload_test \
     -n:20000 -d:2500 \
     "${GNUPG}" \
-    581472 662000 660688 \
-    >"${OFILE}" 2>/dev/null &
-taskset 0x2 "${GNUPG}" --sign --detach-sig "${SFILE}"
+    662000 660688 657783 \
+    &
+taskset 0x2 "${GNUPG}" --sign --detach-sig "${SFILE}" >&2
 
 rm -f "${SFILE}.sig"
